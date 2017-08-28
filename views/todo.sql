4e9224efc7396623abf4d1b061803c387d87b8a2
Roman <roman@romans-mbp.lan>
2017-08-28 17:07:37 -0400

    Fully completed

--- views/todo.sql
+++ views/todo.sql
@@ -8,29 +8,24 @@ CREATE TABLE todo (
   title VARCHAR(255) NOT NULL,                       ///NEEDS TO BE A STRING UP TO 255 CHARACTERS///
    details VARCHAR(1000) ,                          ///HOLDS A LARGE AMOUNT OF TEXT////
   priority INTEGER DEFAULT 1 NOT NULL,
-  created_at NOT NULL DEFAULT 'August 28,2017',
-  completed_at Date,
+  created_at TIMESTAMP NOT NULL,
+  completed_at TIMESTAMP
 );
 
-Write INSERT statements to insert five todos into this table, with one of them completed.
 
-
-INSERT INTO todo (title, details, priority,created_at, completed_at) VALUES ('Wash my car', 'wash and wax', 1, );
-INSERT INTO todo (title, details, priority,created_at, completed_at) VALUES ('Take out the trash', 'before Friday', 2, );
-INSERT INTO todo (title, details, priority,created_at, completed_at) VALUES ('Clean my room', 'vaccum and wipe', 3, );
-INSERT INTO todo (title, details, priority,created_at, completed_at) VALUES ('Clean my kitchen', 'wash the floors', 4, );
-INSERT INTO todo (title, details, priority,created_at, completed_at) VALUES ('Finish my assessments', 'everything outstanding', 5,);
-
-
-
-SELECT title, details FROM todo WHERE graduate = 't';    //////Write a SELECT statement to find all incomplete todos/////
-
-
-SELECT priority FROM todo ORDER BY DESC;      //////Write a SELECT statement to find all todos with a priority above 1.////DONE.
+INSERT INTO todo (title, details, priority,created_at, completed_at) VALUES ('Finish my assessments', 'everything outstanding', 1,'2017-08-28 12:55:32');
+INSERT INTO todo (title, details, priority, created_at) VALUES ('Take out the trash', 'before Friday', 2,'2017-09-28 12:55:32');
+INSERT INTO todo (title, details, priority,created_at) VALUES ('Clean my room', 'vaccum and wipe', 3, '2017-09-28 12:55:32');
+INSERT INTO todo (title, details, priority,created_at) VALUES ('Clean my kitchen', 'wash the floors', 4, '2017-09-28 12:55:32');
+INSERT INTO todo (title, details, priority,created_at) VALUES ('Wash my Car', 'wash and wax', 5, '2017-09-28 12:55:32');
 
 
 
-UPDATE todo SET graduated = 't' WHERE cohort = 11;     /////Write an UPDATE statement to complete one todo by its id. Your ids may differ, so you will choose the id to up.///
+SELECT title FROM todo WHERE completed = NULL;
+
+SELECT priority FROM todo ORDER BY DESC;
+
+UPDATE todo SET completed_at = '2017-08-28 12:55:32' WHERE id = 1;
 
 
-DELETE FROM todo WHERE completed_at IS NULL;    ///Write a DELETE statement to delete all completed todos.///
+DELETE FROM todo WHERE completed_at IS NOT NULL;
